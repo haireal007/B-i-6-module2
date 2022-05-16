00ca9ba171bd56499609a8bf1ea51366b6915102
unknown <hairealmta@gmail.com>
2022-05-17 06:51:38 +0700

    Doi_tuong_hinh_hoc

---
+++ src/Doi_tuong_hinh_hoc/ShapeTest.java
@@ -0,0 +1,11 @@
+package Doi_tuong_hinh_hoc;
+
+public class ShapeTest {
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        System.out.println(shape);
+
+        shape = new Shape("red", false);
+        System.out.println(shape);
+    }
+}
