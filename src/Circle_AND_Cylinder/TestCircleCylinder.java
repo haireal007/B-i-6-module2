01e3088fd079cc5f28381592d3218c2e8692d19b
unknown <hairealmta@gmail.com>
2022-05-17 08:17:09 +0700

    Circle_AND_Cylinder

---
+++ src/Circle_AND_Cylinder/TestCircleCylinder.java
@@ -0,0 +1,8 @@
+package Circle_AND_Cylinder;
+
+public class TestCircleCylinder {
+    public static void main(String[] args){
+        Cylinder cylinder= new Cylinder(2,"Red",3);
+        System.out.println(cylinder.getVolume());
+    }
+}
